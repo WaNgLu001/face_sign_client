76fd1910ad36d623b6bea92655454db656b4f7f2
WaNgLu001 <lg1065849.com>
2020-11-03 10:23:45 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -8,10 +8,8 @@ import './utils/vant'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import echarts from "echarts";
-
 Vue.config.productionTip = false
-// axios.defaults.baseURL = 'http://localhost:9966/'
-axios.defaults.baseURL = 'http://119.3.254.20:9966/'
+axios.defaults.baseURL = 'http://xxx:xxx:xx:xx/'
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios
 Vue.use(axios)
